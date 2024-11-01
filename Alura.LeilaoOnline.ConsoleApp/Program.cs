9822ad8e26337ef9a3f81c6c104f3218bd910c22
Eduardo C F Neto <eduardo@verion.ind.br>
2024-11-01 17:40:45 -0300

    1. Promovendo rápido feedback: - Estrutura de um teste automatizado
    CheckPoint

--- Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -4,8 +4,9 @@ namespace Alura.LeilaoOnline.ConsoleApp
 {
     class Program
     {
-        static void Main()
+        private static void LeilaoComVariosLen()
         {
+            //Arrange - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             var maria = new Interessada("Maria", leilao);
@@ -15,9 +16,25 @@ namespace Alura.LeilaoOnline.ConsoleApp
             leilao.RecebeLance(fulano, 1000);
             leilao.RecebeLance(maria, 990);
 
+            //Act - método sob teste
             leilao.TerminaPregao();
 
-            Console.WriteLine(leilao.Ganhador.Valor);
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            if (valorEsperado == valorObtido)
+            {
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.WriteLine("TESTE FALHOU");
+            }
+        }
+
+        static void Main()
+        {
         }
     }
 }
