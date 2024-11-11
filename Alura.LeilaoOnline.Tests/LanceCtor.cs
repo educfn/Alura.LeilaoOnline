6ad05211db297cc07154188f9d73d979a6ae4c27
Eduardo C F Neto <eduardo@verion.ind.br>
2024-11-11 17:42:33 -0300

    Estudar Unit testing - 4.Testando todos os comportamentos - Testando outra exceção

---
+++ Alura.LeilaoOnline.Tests/LanceCtor.cs
@@ -0,0 +1,26 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            //Arrange - cenário
+            var valorNegativo = -100;
+
+        //Assert
+        Assert.Throws<System.ArgumentException>(
+
+            //Act
+            () => new Lance(null, valorNegativo)
+        );
+        }
+    }
+}
